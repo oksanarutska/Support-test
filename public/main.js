850eee0122357e967e1533cbc352847751365477
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2020-01-16 23:02:33 +0200

    added and fixed elements

--- public/main.js
+++ public/main.js
@@ -1,24 +1,40 @@
-// SLICK SLIDER
-
-$('.single-item').slick({
-    arrows: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    speed: 1000,
-    fade: true,
-    cssEase: 'linear'
-
-});
-
 $(document).ready(function() {
-    $(".menu li").hover(
-        function() {
-            $(this).children('ul').hide();
-            $(this).children('ul').slideDown('fast');
-        },
-        function() {
-            $('ul', this).slideUp('fast');
+    // SLICK SLIDER
+
+    $('.single-item').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear'
+
+    });
+    $(".menu li").click(
+        function(e) {
+            const $item = $(this);
+            const isOpened = $item.data().opened;
+
+            if (isOpened) {
+                $(this).children('ul').slideUp('fast');
+            } else {
+                $(".menu li").data('opened', false)
+                $(".menu li").children('ul').slideUp('fast');
+
+                $(this).children('ul').hide();
+                $(this).children('ul').slideDown('fast');
+            }
+
+            $item.data('opened', !isOpened);
         });
+
+    $('.navbar-toggler')
+        .click(function(e) {
+            let $btn = $(this);
+            let $target = $($btn.data().target);
+
+            $target.toggleClass('show-m')
+        })
 });
